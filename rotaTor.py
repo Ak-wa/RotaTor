a416621b6dc89a40c82f1c62644245db238397e3
TheClockTwister <leon.wiesen@rwth-aachen.de>
2019-05-23 17:12:21 +0200

    added verbosity

--- rotaTor.py
+++ rotaTor.py
@@ -11,19 +11,23 @@ from requests import get
 
 
 class Rotator:
-    def __init__(self, tor_host="127.0.0.1", tor_port=9150):
+    def __init__(self, tor_host="127.0.0.1", tor_port=9150, verbose=False):
         self.__controller = Controller.from_port(port=9051)
         self.__tor_host = tor_host
         self.__tor_port = tor_port
+        self.__verbose = verbose
+        self.node = None
 
-        self.public_ip = get("https://myexternalip.com/raw")
-        print("Your (real) public IP address is:", self.public_ip)
+        if self.__verbose:
+            self.public_ip = get("https://myexternalip.com/raw").text
+            print("Your (real) public IP address is:", self.public_ip)
 
     def rotate(self):
         self.__controller.authenticate()
         socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, self.__tor_host, self.__tor_port)
         socket.socket = socks.socksocket
-        ip_req = get("https://myexternalip.com/raw")
-        print("[+] Exit node: %s" % ip_req.text)
+        self.node = get("https://myexternalip.com/raw").text
+        if self.__verbose:
+            print("[+] Exit node: %s" % self.node)
         self.__controller.signal(Signal.NEWNYM)
         sleep(self.__controller.get_newnym_wait())
