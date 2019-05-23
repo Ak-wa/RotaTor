3038ac663da30576d74505b9e09a9f9075796fe9
TheClockTwister <leon.wiesen@rwth-aachen.de>
2019-05-23 15:47:52 +0200

    Format improvements

--- rotaTor.py
+++ rotaTor.py
@@ -2,31 +2,34 @@
 # for any bugs/issues contact me at github.com/ak-wa
 # woop woop
 try:
-    import socks, socket
+    import socks
+    import socket
     from time import sleep
     from stem.control import Controller
     from stem import Signal
     from sys import stdout
     from requests import get
-except:
+except ImportError:
     from os import system
+
     system('pip install socks')
     system('pip install stem')
     system('pip install requests')
-    import socks, socket
+    import socks
+    import socket
     from time import sleep
     from stem.control import Controller
     from stem import Signal
     from requests import get
 
-with Controller.from_port(port = 9051) as controller:
+with Controller.from_port(port=9051) as controller:
     controller.authenticate()
     socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, "127.0.0.1", 9150)
-    socket.socket = socks.socksocket     
+    socket.socket = socks.socksocket
     while 1:
         ip_req = get("https://myexternalip.com/raw")
-        print("[+] Exit node: %s" % (ip_req.text))
-        print("[+] Your code could happen here!") # You can delete this
+        print("[+] Exit node: %s" % ip_req.text)
+        print("[+] Your code could happen here!")  # You can delete this
         # Here comes your code, e.g a request to a website
         #
         #
