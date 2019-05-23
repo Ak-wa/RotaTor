adebf0f92807cb3ceb614c1aba4654d072bbd13a
TheClockTwister <leon.wiesen@rwth-aachen.de>
2019-05-23 17:05:36 +0200

    added public IP feature

--- rotaTor.py
+++ rotaTor.py
@@ -16,6 +16,9 @@ class Rotator:
         self.__tor_host = tor_host
         self.__tor_port = tor_port
 
+        self.public_ip = get("https://myexternalip.com/raw")
+        print("Your (real) public IP address is:", self.public_ip)
+
     def rotate(self):
         self.__controller.authenticate()
         socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, self.__tor_host, self.__tor_port)
