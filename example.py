38692951d5bddd8a372458ed44c33a1273a490b1
Ak <44728139+Ak-wa@users.noreply.github.com>
2020-10-05 10:43:41 +0200

    Example edit

--- example.py
+++ example.py
@@ -2,4 +2,5 @@ from rotaTor import Rotator
 rot = Rotator(verbose=False)
 for i in range(3):
     rot.rotate()
-    print(rot.get_node())
+    print("[+] Your code could happen here!")
+    print("[+] Current IP: " + rot.get_node())
